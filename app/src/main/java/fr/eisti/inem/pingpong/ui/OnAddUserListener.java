5259f56ca8ac360d0f2713a19cbf6cf32e33456f
dumurtipha <dumurtipha@eisti.eu>
2018-11-07 14:02:38 +0100

    [UPDATE] AddPlayerDisplay

--- app/src/main/java/fr/eisti/inem/pingpong/ui/OnAddUserListener.java
+++ app/src/main/java/fr/eisti/inem/pingpong/ui/OnAddUserListener.java
@@ -2,9 +2,11 @@ package fr.eisti.inem.pingpong.ui;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import fr.eisti.inem.pingpong.MainActivity;
 import fr.eisti.inem.pingpong.R;
@@ -27,14 +29,28 @@ public class OnAddUserListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        final TextView titlePseudo  = new TextView(this.ma);
         final EditText inputPseudo  = new EditText(this.ma);
+        final TextView titleName  = new TextView(this.ma);
         final EditText inputName    = new EditText(this.ma);
+        final TextView titleSurname  = new TextView(this.ma);
         final EditText inputSurname = new EditText(this.ma);
         final LinearLayout ll       = new LinearLayout(this.ma);
 
+
+        inputPseudo.setInputType(InputType.TYPE_CLASS_TEXT);
+        titlePseudo.setText(R.string.titlePseudo);
+        inputName.setInputType(InputType.TYPE_CLASS_TEXT);
+        titleName.setText(R.string.titleName);
+        inputSurname.setInputType(InputType.TYPE_CLASS_TEXT);
+        titleSurname.setText(R.string.titleSurname);
+
         ll.setOrientation(LinearLayout.VERTICAL);
+        ll.addView(titlePseudo);
         ll.addView(inputPseudo);
+        ll.addView(titleName);
         ll.addView(inputName);
+        ll.addView(titleSurname);
         ll.addView(inputSurname);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this.ma);
@@ -45,7 +61,7 @@ public class OnAddUserListener implements View.OnClickListener {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         User user = new User(inputPseudo.getText().toString(),
-                                null,
+                                new String(),
                                 inputName.getText().toString(),
                                 inputSurname.getText().toString());
                         try {
@@ -54,7 +70,8 @@ public class OnAddUserListener implements View.OnClickListener {
                             e.printStackTrace();
                         }
                     }
-                });
+                })
+                .show();
 
         //TODO import picture
     }
