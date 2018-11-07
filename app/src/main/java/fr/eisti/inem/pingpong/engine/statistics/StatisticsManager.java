fb5103407edfe2cefe624aa4c9725e1c25aacf70
Clément Aubin <aubincleme@gmail.com>
2018-11-07 13:58:18 +0100

    Move managers in their own packages

--- app/src/main/java/fr/eisti/inem/pingpong/engine/statistics/StatisticsManager.java
+++ app/src/main/java/fr/eisti/inem/pingpong/engine/statistics/StatisticsManager.java
@@ -1,6 +1,8 @@
-package fr.eisti.inem.pingpong.engine;
+package fr.eisti.inem.pingpong.engine.statistics;
 
 import android.content.Context;
+
+import fr.eisti.inem.pingpong.engine.AbstractManager;
 
 public class StatisticsManager extends AbstractManager {
     public StatisticsManager(Context context) {
