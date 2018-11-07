64d1fb476cfec874a0ac3453b33960fdae387cba
dumurtipha <dumurtipha@eisti.eu>
2018-11-07 16:51:04 +0100

    [ADD] newGame player selection activity

--- app/src/main/java/fr/eisti/inem/pingpong/NewGamePlayerSelect.java
+++ app/src/main/java/fr/eisti/inem/pingpong/NewGamePlayerSelect.java
@@ -1,4 +1,27 @@
 package fr.eisti.inem.pingpong;
 
-public class NewGamePlayerSelect {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eisti.inem.pingpong.engine.EngineManager;
+import fr.eisti.inem.pingpong.engine.user.User;
+
+public class NewGamePlayerSelect extends AppCompatActivity {
+
+    private EngineManager engineManager;
+    private List<User> playerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game_player_selection);
+        this.engineManager = EngineManager.get();
+        this.playerList = new ArrayList<>();
+    }
+
+
+
 }
